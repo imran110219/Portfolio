b5cbec7e2ba7d1bed13023c1b5764c43a8b26d7a
imran110219 <imran110219@gmail.com>
2019-05-15 03:08:49 +0600

    add

--- materialize-resume/js/init.js
+++ materialize-resume/js/init.js
@@ -5,3 +5,15 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
