42492a347497796e8128272ea9e48dd803367525
imran110219 <imran110219@gmail.com>
2019-05-24 12:03:10 +0600

    add

--- materialize-resume/js/init.js
+++ materialize-resume/js/init.js
@@ -1,5 +1,5 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.sidenav').sidenav();
 
@@ -7,13 +7,40 @@
 })(jQuery); // end of jQuery name space
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.slider');
-    var instances = M.Slider.init(elems, options);
-  });
+document.addEventListener('DOMContentLoaded', function (options) {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, options);
+});
 
-  // Or with jQuery
+// Or with jQuery
 
-  $(document).ready(function(){
-    $('.slider').slider();
-  });
+$(document).ready(function () {
+  $('.slider').slider();
+});
+
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 150) {
+    $('.site-navigation').css('z-index', 10);
+  }
+  else {
+    $('.site-navigation').css('z-index', 0);    
+  }
+});
+
+//Function for setting fullscreen slider
+
+function SetSlidersDimension(){
+  var y = $(window).height();
+  $('.slider').css('height', y+'px');  
+  $('.slides').css('height', y+'px');
+}
+
+$(window).load(function(){
+  SetSlidersDimension();
+})
+
+$(window).resize(function(){
+  SetSlidersDimension();
+});
+
